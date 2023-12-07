333af9053aa56f65a60736ae4c425f2f86cff09a
Rafael Porfirio Barros <rafaporfirio.barros@gmail.com>
2023-12-07 11:46:21 -0300

    Upload Estruturando o codigo Final

---
+++ extraindo_dados.py
@@ -0,0 +1,17 @@
+from dados_repos import DadosRepositorios
+
+amazon_rep = DadosRepositorios('amzn')
+ling_mais_usadas_amzn = amazon_rep.cria_df_linguagens()
+#print(ling_mais_usadas_amzn)
+
+netflix_rep = DadosRepositorios('netflix')
+ling_mais_usadas_netflix = netflix_rep.cria_df_linguagens()
+
+spotify_rep = DadosRepositorios('spotify')
+ling_mais_usadas_spotify = spotify_rep.cria_df_linguagens()
+
+# Salvando os dados 
+
+ling_mais_usadas_amzn.to_csv('dados/linguagens_amzn.csv')
+ling_mais_usadas_netflix.to_csv('dados/linguagens_netflix.csv')
+ling_mais_usadas_spotify.to_csv('dados/linguagens_spotify.csv')
